1c82d4a3106e1e92315481f6f29ebedcedefe15b
Parham Alvani <parham.alvani@gmail.com>
2016-02-17 00:21:08 +0330

    Response time measuring was added.

--- src/session.c
+++ src/session.c
@@ -14,6 +14,7 @@
 #include <libsoup/soup.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 
 #include "session.h"
 
@@ -28,10 +29,14 @@ int session_new_connection(const char *method, const char *url)
 {
 	SoupMessage *msg;
 	uint32_t status;
+	time_t t1, t2;
 
 	msg = soup_message_new(method, url);
+	time(&t1);
 	status = soup_session_send_message(session, msg);
+	time(&t2);
 	
+	printf("Response time: %lus\n", t2 - t1);
 	printf("Status: %d\n", status);
 	printf("Server: %s\n", soup_message_headers_get_one(msg->response_headers, "Server"));
 
