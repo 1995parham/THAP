1547b868c67046af3e4e76f9d5a2d415a4da61d8
Parham Alvani <parham.alvani@gmail.com>
2016-02-20 02:45:00 +0330

    Basic functionality for authentication was added.

---
+++ src/auth.c
@@ -0,0 +1,56 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : auth.c
+ *
+ * [] Creation Date : 20-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#include <libsoup/soup.h>
+#include <glib.h>
+#include <stdio.h>
+
+#include "auth.h"
+
+static GHashTable *a;
+
+static void auth_init(void)
+{
+	a = g_hash_table_new(g_direct_hash, g_direct_equal);
+}
+
+void auth_callback(SoupSession *session, SoupMessage *msg,
+		SoupAuth *auth, gboolean retrying,
+		gpointer user_data)
+{
+	if (!a)
+		auth_init();
+	int index = *(int *) user_data;
+
+	index++;
+	g_hash_table_insert(a, GINT_TO_POINTER(index), auth);
+}
+
+SoupAuth *auth_get(int index)
+{
+	if (!a)
+		auth_init();
+	return g_hash_table_lookup(a, GINT_TO_POINTER(index));
+}
+
+void auth_authenticate(int index, const char *username, const char *password)
+{
+	if (!a)
+		auth_init();
+	SoupAuth *auth = NULL;
+
+	auth = g_hash_table_lookup(a, GINT_TO_POINTER(index));
+	if (!auth)
+		return;
+}
---
+++ src/auth.h
@@ -0,0 +1,24 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : auth.h
+ *
+ * [] Creation Date : 20-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#ifndef AUTH_H
+#define AUTH_H
+
+void auth_callback(SoupSession *session, SoupMessage *msg,
+		SoupAuth *auth, gboolean retrying,
+		gpointer user_data);
+
+SoupAuth *auth_get(int index);
+
+#endif
